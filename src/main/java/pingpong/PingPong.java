7fe6ced2f7bfa9efa95cbddf8e807494fb994283
Uka0001 <ukraine0001@gmail.com>
2022-09-08 23:30:26 +0300

    working code v 1.0 added

---
+++ src/main/java/pingpong/PingPong.java
@@ -0,0 +1,28 @@
+package pingpong;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class PingPong implements Runnable {
+    private final String word;
+    private final Object monitor;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronizedPrint(i);
+        }
+    }
+
+    private void synchronizedPrint(int i) {
+        synchronized (monitor) {
+            System.out.println(word + i);
+            try {
+                monitor.notifyAll();
+                monitor.wait(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
